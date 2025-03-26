2287902dca597700074b30052d7d6865e77db7c5
Mark Heckler <mark.heckler@gmail.com>
2025-03-26 16:10:38 -0500

    Initial commit

---
+++ src/main/java/com/thehecklers/BlogGenAI/BlogGenAiApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.BlogGenAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogGenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogGenAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thehecklers/BlogGenAI/BlogGenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehecklers.BlogGenAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogGenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
